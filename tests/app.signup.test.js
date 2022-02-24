78d3cf690dab070114cff0ba24bbeb57589a0978
m1brah1m <mi.mostafaibrahim@gmail.com>
2022-02-24 21:39:48 +0200

    :test_tube: Added more test cases [/signup]

--- tests/app.signup.test.js
+++ tests/app.signup.test.js
@@ -16,4 +16,80 @@ describe("POST", () => {
         expect(response.body).toEqual({ message: "Account created" });
       });
   });
+  test("/signup --> Invalid create an account [2]", () => {
+    return request(app)
+      .post("/signup")
+      .send({
+        username: "Mostafa",
+        email: "mostaf.com",
+        password: "1234",
+      })
+      .expect(400)
+      .expect("Content-type", /json/)
+      .then((response) => {
+        expect(response.body).toEqual({ message: "Invalid Email" });
+      });
+  });
+  test("/signup --> failed create an account", () => {
+    return request(app)
+      .post("/signup")
+      .send({
+        username: "Mostafa",
+        email: "mostafa@example.com",
+        password: "1234",
+      })
+      .expect(400)
+      .expect("Content-type", /json/)
+      .then((response) => {
+        expect(response.body).toEqual({ message: "Email in use" });
+      });
+  });
+  test("/signup --> failed create an account [missing username]", () => {
+    return request(app)
+      .post("/signup")
+      .send({
+        email: "mostafa2@example.com",
+        password: "1234",
+      })
+      .expect(400)
+      .expect("Content-type", /json/)
+      .then((response) => {
+        expect(response.body).toEqual({ message: "Missing fields" });
+      });
+  });
+  test("/signup --> failed create an account [missing email]", () => {
+    return request(app)
+      .post("/signup")
+      .send({
+        username: "Mostafa",
+        password: "1234",
+      })
+      .expect(400)
+      .expect("Content-type", /json/)
+      .then((response) => {
+        expect(response.body).toEqual({ message: "Missing fields" });
+      });
+  });
+  test("/signup --> failed create an account [missing password]", () => {
+    return request(app)
+      .post("/signup")
+      .send({
+        username: "Mostafa",
+        email: "mostafa3@example.com",
+      })
+      .expect(400)
+      .expect("Content-type", /json/)
+      .then((response) => {
+        expect(response.body).toEqual({ message: "Missing fields" });
+      });
+  });
+  test("/signup --> failed create an account [missing all]", () => {
+    return request(app)
+      .post("/signup")
+      .expect(400)
+      .expect("Content-type", /json/)
+      .then((response) => {
+        expect(response.body).toEqual({ message: "Missing fields" });
+      });
+  });
 });
