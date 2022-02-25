dcbbd13d4686f3ae0de2d74e0ea328d43715984d
m1brah1m <mi.mostafaibrahim@gmail.com>
2022-02-25 22:20:30 +0200

    :passport_control: Added verifyAccessToken

--- src/services/auth.service.js
+++ src/services/auth.service.js
@@ -2,11 +2,14 @@ const jwt = require("jsonwebtoken");
 const config = require("../config/env.config");
 
 const createAccessToken = (user) => {
-  console.log("in fn", user.dataValues.id);
   const token = jwt.sign({ id: user.dataValues.id }, config.JWT_KEY, {
     expiresIn: config.JWT_EXPIRES_IN,
   });
   return token;
 };
 
-module.exports = createAccessToken;
+const verifyAccessToken = (token) => {
+  const match = jwt.verify(token, config.JWT_KEY);
+  return match;
+};
+module.exports = { createAccessToken, verifyAccessToken };
