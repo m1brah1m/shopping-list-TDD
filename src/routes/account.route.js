2ee5a2c0f777a19d02fcb26d90575bc3d76d0d5b
m1brah1m <mi.mostafaibrahim@gmail.com>
2022-02-27 23:11:01 +0200

    :green_circle: Added updateAccount controller to router

--- src/routes/account.route.js
+++ src/routes/account.route.js
@@ -1,6 +1,9 @@
-const { getAccount } = require("../controllers/account.controller");
+const {
+  getAccount,
+  updateAccount,
+} = require("../controllers/account.controller");
 const router = require("express").Router();
 
-router.route("/").get(getAccount);
+router.route("/").get(getAccount).put(updateAccount);
 
 module.exports = router;
