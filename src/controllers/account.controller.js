a0aa03b132460795c8936c0873da517650fa31cb
m1brah1m <mi.mostafaibrahim@gmail.com>
2022-02-27 23:10:06 +0200

    :green_circle: Added updateAccount controller

--- src/controllers/account.controller.js
+++ src/controllers/account.controller.js
@@ -1,3 +1,6 @@
+const User = require("../models/user.model");
+const { hashPassword } = require("../services/user.service");
+
 const getAccount = (req, res, next) => {
   try {
     return res.status(200).json({
@@ -11,5 +14,15 @@ const getAccount = (req, res, next) => {
     return res.status(500).json({ message: "Error" });
   }
 };
-
-module.exports = { getAccount };
+const updateAccount = async (req, res, next) => {
+  try {
+    if (req.body.password) {
+      req.body.password = await hashPassword(req.body.password);
+    }
+    const user = await User.update(req.body, { where: { id: req.user.id } });
+    return res.status(200).json({ message: "Updated" });
+  } catch (error) {
+    return res.status(500).json({ message: "Error" });
+  }
+};
+module.exports = { getAccount, updateAccount };
