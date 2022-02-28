297d6a4f71c8309ed7829fbc3fe3879e761ad766
m1brah1m <mi.mostafaibrahim@gmail.com>
2022-02-28 20:59:52 +0200

    :card_file_box: Added item model

---
+++ src/models/item.model.js
@@ -0,0 +1,33 @@
+const { DataTypes } = require("sequelize");
+const bcrypt = require("bcrypt");
+const sequelize = require("../config/db");
+
+//item's name
+//items' status (Check,In progress)
+//userId
+const Item = sequelize.define("items", {
+  id: {
+    field: "id",
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    autoIncrement: true,
+  },
+  itemName: {
+    field: "item_name",
+    type: DataTypes.STRING(50),
+    allowNull: false,
+  },
+  itemStatus: {
+    field: "item_status",
+    type: DataTypes.STRING(12),
+    allowNull: false,
+  },
+  userId: {
+    field: "user_id",
+    type: DataTypes.INTEGER,
+    allowNull: false,
+  },
+});
+
+Item.sync();
+module.exports = Item;
