6ed9c79cb60477307b40aec2c7691e2be241bd6b
m1brah1m <mi.mostafaibrahim@gmail.com>
2022-02-28 21:04:27 +0200

    :green_circle: Added /shoppinglist/items route

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const signUpRoute = require("./routes/signup.route");
 const logInRoute = require("./routes/login.route");
 const accountRoute = require("./routes/account.route");
+const shoppinglistRoute = require("./routes/shoppinglist.route");
 const protect = require("./middlewares/protect.middleware");
 const app = express();
 
@@ -10,4 +11,5 @@ app.use(express.json());
 app.use("/signup", signUpRoute);
 app.use("/login", logInRoute);
 app.use("/account", protect, accountRoute);
+app.use("/shoppinglist/items", protect, shoppinglistRoute);
 module.exports = app;
